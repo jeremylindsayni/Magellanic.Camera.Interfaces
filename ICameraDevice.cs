ae6591887ceb59a89b28238386e77d4c56a91d86
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-05-22 16:57:51 +0100

    Initial commit of camera interface

---
+++ ICameraDevice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+
+namespace Magellanic.Camera.Interfaces
+{
+    public interface ICameraDevice : IDisposable
+    {
+        MediaCapture ViewFinder { get; set; }
+
+        Task<DeviceInformation> GetCameraAtPanelLocation(Panel cameraLocation);
+        
+        Task<DeviceInformation> GetDefaultCamera();
+
+        Task InitialiseCameraAsync(DeviceInformation cameraToInitialise);
+    }
+}
